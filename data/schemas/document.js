5b0e79bfffc687b6ab89aa9c98ab19a62fe763d9
Allen Dolph <allendolph@gmail.com>
2014-01-09 10:26:03 -0800

    added new structure for mongoose model and schemas

---
+++ data/schemas/document.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: allendolph
+ * Date: 1/9/14
+ * Time: 10:24 AM
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ data/schemas/user.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: allendolph
+ * Date: 1/9/14
+ * Time: 10:25 AM
+ * To change this template use File | Settings | File Templates.
+ */
